2eccc3d901b908b6b6ed3c6ce1af94d129786bef
IgnisFowl <fowl.ignis@gmail.com>
2025-11-10 18:45:30 -0300

    fixed double assignment on minimap_fov_utils

--- src/minimap_fov_utils.c
+++ src/minimap_fov_utils.c
@@ -23,14 +23,13 @@ void	calc_fov_edges(t_game *g, t_fov *f)
 	double	len;
 
 	len = 2 * TILE_SIZE;
-
 	f->end_left_x = f->player_px + (int)((g->dir_x + g->plane_x) * len);
 	f->end_left_y = f->player_py + (int)((g->dir_y + g->plane_y) * len);
 	f->end_right_x = f->player_px + (int)((g->dir_x - g->plane_x) * len);
 	f->end_right_y = f->player_py + (int)((g->dir_y - g->plane_y) * len);
 }
 
-void fov_to_triangle(t_fov *f, t_triangle *t)
+void	fov_to_triangle(t_fov *f, t_triangle *t)
 {
 	t->x0 = f->player_px;
 	t->y0 = f->player_py;
@@ -40,10 +39,12 @@ void fov_to_triangle(t_fov *f, t_triangle *t)
 	t->y2 = f->end_right_y;
 }
 
-void tri_bounds(t_triangle *t, t_tri_bounds *b)
+void	tri_bounds(t_triangle *t, t_tri_bounds *b)
 {
-	b->min_x = b->max_x = t->x0;
-	b->min_y = b->max_y = t->y0;
+	b->min_x = t->x0;
+	b->max_x = t->x0;
+	b->min_y = t->y0;
+	b->max_y = t->y0;
 	if (t->x1 < b->min_x)
 		b->min_x = t->x1;
 	if (t->x2 < b->min_x)
@@ -62,18 +63,20 @@ void tri_bounds(t_triangle *t, t_tri_bounds *b)
 		b->max_y = t->y2;
 }
 
-int point_in_tri(t_triangle *t, int x, int y)
+int	point_in_tri(t_triangle *t, int x, int y)
 {
-	int 	d;
-	float 	a;
+	int		d;
+	float	a;
 	float	b;
 	float	c;
 
 	d = (t->y1 - t->y2) * (t->x0 - t->x2) + (t->x2 - t->x1) * (t->y0 - t->y2);
 	if (!d)
 		return (0);
-	a = ((t->y1 - t->y2) * (x - t->x2) + (t->x2 - t->x1) * (y - t->y2)) / (float)d;
-	b = ((t->y2 - t->y0) * (x - t->x2) + (t->x0 - t->x2) * (y - t->y2)) / (float)d;
+	a = ((t->y1 - t->y2) * (x - t->x2) + \
+(t->x2 - t->x1) * (y - t->y2)) / (float)d;
+	b = ((t->y2 - t->y0) * (x - t->x2) + \
+(t->x0 - t->x2) * (y - t->y2)) / (float)d;
 	c = 1 - a - b;
 	return (a >= 0 && b >= 0 && c >= 0);
 }
